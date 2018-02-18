44a3fea43835bb38bf9b76dd548f1a8db1a58691
long1long <long1long@gmail.com>
2018-02-18 11:04:48 +0800

    idapython

--- IDAPython/create_bap_table.py
+++ IDAPython/create_bap_table.py
@@ -10,22 +10,29 @@ db = MySQLdb.connect("192.168.178.1","root","123456","bap" )
 cursor = db.cursor()
 
 # drop the table
-cursor.execute("DROP TABLE IF EXISTS BAP")
-
+cursor.execute("DROP TABLE IF EXISTS task")
+cursor.execute("DROP TABLE IF EXISTS status")
 # create table BAP
-sql = """CREATE TABLE BAP (
+sql = """CREATE TABLE task (
          old_sample_num  INT,
          new_sample_num  INT,
-         convert_address INT  
+         convert_address INT,
+         done INT,
+         PRIMARY KEY (convert_address)
           )"""
 
 cursor.execute(sql)
-
+sql = """CREATE TABLE status (
+         convert_address  INT,
+         convert_status  INT,
+         PRIMARY KEY (convert_address, convert_status)
+          )"""
+cursor.execute(sql)
 # initial the BAP table
-cursor.execute("INSERT INTO BAP(old_sample_num,new_sample_num,convert_address) VALUES(1,2,0x1a)")
+cursor.execute("INSERT INTO task(old_sample_num,new_sample_num,convert_address,done) VALUES(1,0,0,0);")
 db.commit()
 
-cursor.execute("SELECT * FROM BAP")
+cursor.execute("SELECT * FROM task")
  
 # 
 rows = cursor.fetchall()
--- IDAPython/myrender.py
+++ IDAPython/myrender.py
@@ -3,36 +3,105 @@ from idaapi import *
 from idc import *
 import MySQLdb
 import os
-trace_bbl = []
+run_base_addr=0
+g_new_num=2
+
 def color_trace(filename):
+    global run_base_addr
     old_sample_num=int(filename.split('-')[0],10)
     mybase_addr = get_imagebase()
+    db = MySQLdb.connect("192.168.178.1","root","123456","bap" )
+    cursor = db.cursor()
     # read trace file
     f_trace = open(filename,'r')
     for line in f_trace.readlines():
         #get trace address list
+        if(run_base_addr==0):
+            run_base_addr=int(line,16)
+            continue
         tmp_addr=int(line.split()[0],16)+mybase_addr
         tmp_taken=int(line.split()[1],10)
         if(tmp_taken==1):
-            mycolor=old_sample_num|0x80000000
+            mycolor=old_sample_num|0x80000000  
         else:
-            mycolor=old_sample_num
+            mycolor=old_sample_num   
         print hex(tmp_addr)+' '+hex(mycolor)
+        sql_cmd='INSERT IGNORE INTO status(convert_address,convert_status) VALUES('+str(tmp_addr-run_base_addr)+','+str(tmp_taken)+')' 
+        cursor.execute(sql_cmd)
+        db.commit()
         set_color(tmp_addr,CIC_ITEM,mycolor)
+    db.close()
 
-def my_db_insert(old_sample_num,new_sample_num,convert_address): #int int int
+
+
+def get_task():
     db = MySQLdb.connect("192.168.178.1","root","123456","bap" )
     cursor = db.cursor()
-    sql_cmd='INSERT INTO BAP(old_sample_num,new_sample_num,convert_address) VALUES('+str(old_sample_num)+','+str(new_sample_num)+','+hex(convert_address)+')'
+    sql_cmd='select * from task where done=1;'
+    cursor.execute(sql_cmd)
+    data = cursor.fetchone()
+    print data
+    #print type(data)
+    db.close()
+    return data
+def set_task(old_sample_num,new_sample_num,convert_addr): #finish color
+    db = MySQLdb.connect("192.168.178.1","root","123456","bap" )
+    cursor = db.cursor()
+    sql_cmd='UPDATE task SET done=2 WHERE (old_sample_num=%d and new_sample_num=%d) and convert_address=%x;' %(old_sample_num,new_sample_num,convert_addr)
+    print sql_cmd
     cursor.execute(sql_cmd)
     db.commit()
     db.close()
 
-os.chdir('/Users/longlong/VirtualBox VMs/ubuntu14-disk/IDAPython/')
-color_trace('1-addrs.txt')
-#my_db_insert(1,3,0x1b)
+def insert_task(old_sample_num):
+    global g_new_num
+    # get the convert_addr
+    db = MySQLdb.connect("192.168.178.1","root","123456","bap" )
+    cursor = db.cursor()
+    set_0=set()
+    set_1=set()
+    addr_0_cmd='select convert_address from status where convert_status=0'
+    addr_1_cmd='select convert_address from status where convert_status=1'
+    cursor.execute(addr_0_cmd)
+    addr_0 = cursor.fetchall()
+    for addr in addr_0:
+        set_0.add(addr[0])
+    print set_0
+    cursor.execute(addr_1_cmd)
+    addr_1 = cursor.fetchall()
+    for addr in addr_1:
+        set_1.add(addr[0])
+    print set_1
+    set_2=set_0^set_1
+    print set_2
+    for convert_addr in set_2:
+        sql_cmd='INSERT INTO task(old_sample_num,new_sample_num,convert_address,done) VALUES('+str(old_sample_num)+','+str(g_new_num)+','+str(convert_addr)+','+str(0)+');'
+        g_new_num=g_new_num+1
+        cursor.execute(sql_cmd)
+        db.commit()
+    
+    db.close()
 
-#color_trace('/Users/longlong/VirtualBox VMs/ubuntu14-disk/IDAPython/1-1-addrs.txt',0xff)
+def main():
+    os.chdir('/Users/longlong/VirtualBox VMs/ubuntu14-disk/share')
+    #get first task
+    task_data=get_task()
+    if(task_data):
+        old_sample_num=task_data[0]
+        new_sample_num=task_data[1]
+        convert_addr=task_data[2]
+        print 'old_sample_num:%d,new_sample_num:%d,convert_addr:%x' %(old_sample_num,new_sample_num,convert_addr)
+        set_task(old_sample_num,new_sample_num,convert_addr)
+        color_trace('%d-addrs.txt' %old_sample_num)
+        insert_task(old_sample_num)
+    else:
+        print 'no first task,please run the schedule.py'
+
+    
+main()
+#insert_task(1)
+print 'ok'
+
 
 
 
