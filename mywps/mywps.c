bc1a74add921b79d0f68e4bc34ef7612a95cdf8f
long1long <long1long@gmail.com>
2018-03-03 11:04:19 +0800

    mystrcmp

--- mywps/mywps.c
+++ mywps/mywps.c
@@ -5,7 +5,17 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <string.h>
-
+int mystrcmp(const char *dest, const char *source)  
+{  
+   //assert((NULL != dest) && (NULL != source));  
+   while (*dest && *source && (*dest == *source))  
+  {  
+          dest ++;  
+          source ++;  
+  }  
+   return *dest - *source;  
+  
+}  
 int main(int argc, char **argv)
 {
   int  fd;
@@ -16,12 +26,16 @@ int main(int argc, char **argv)
   fd = open(argv[1], O_RDONLY);
   read(fd, buff, 256);
   close(fd);
-  /*
-  if(strcmp(buff,"lon\n")==0)
+  
+  if(mystrcmp(buff,"lon\n")==0)
 {
   printf("long\n");
   return 1;
-}*/
+}
+else{
+  printf("nocmp\n");
+}
+/*
   if (buff[0] == 'a')
 {
   printf("buffer[0] is a\n");
@@ -67,7 +81,7 @@ else
   {
     printf("control flow\n");
   }
-  
+*/
   return 0;
 }
 
